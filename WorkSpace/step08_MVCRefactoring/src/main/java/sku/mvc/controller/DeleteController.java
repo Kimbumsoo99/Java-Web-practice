7cd7f809091a40634e1cd051b05d1ee3b8d62206
Kimbumsoo99 <show7441@naver.com>
2023-01-18 12:41:32 +0900

    MVC 연습

---
+++ WorkSpace/step08_MVCRefactoring/src/main/java/sku/mvc/controller/DeleteController.java
@@ -0,0 +1,24 @@
+package sku.mvc.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DeleteController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("DeleteController 호출되었습니다.");
+		
+		//service-> dao호출해서 삭제완료된후 이동
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("index.jsp");
+		mv.setRedirect(true);
+		
+		return mv;
+	}
+
+}
